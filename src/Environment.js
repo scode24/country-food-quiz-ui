cb54fa0d8780aa55e1faed1bf063556decd09866
Soumyabrata Sarkar <sarkar.soumyabrata2@gmail.com>
2022-04-01 20:17:16 +0530

    Update service api url

--- src/Environment.js
+++ src/Environment.js
@@ -1,4 +1,4 @@
-export const publicApiBase = 'https://country-food-quiz-service.herokuapp.com';
+export const publicApiBase = 'https://country-food-quiz-service.herokuapp.com/v1';
 export const allowedMaxWrongCount = 2;
 export const negativeScore = 2;
 export const positiveScore = 5;
